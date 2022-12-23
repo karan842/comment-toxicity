8f313e431e8da860220dbc273493e2d3c9907f5b
Karan <karanshingde@gmail.com>
2022-12-23 14:48:04 +0530

    Dev - 1

--- app.py
+++ app.py
@@ -8,28 +8,27 @@ import pandas as pd
 import numpy as np
 import json
 import re
-import text
 import tensorflow as tf
 import tensorflow_text as text
+import tensorflow_hub as hub
 from flask import Flask
 
+## Loading tf model
+
+model_path = 'saved_models\comment-toxicity-bert.h5'
+
+load_model = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
+
+model = tf.keras.models.load_model(model_path, 
+                                   custom_objects={hub:'KerasLayer'},
+                                   options=load_model)
+print("Model loaded Successfully!")
 
 app = Flask(__name__)
 
 @app.route('/admin')
 def hello_admin():
     return 'Hello Admin'
-
-@app.route('/guest/<guest>')
-def hello_guest(guest):
-    return 'Hello %S as Guest ' % guest
-
-@app.route('/user/<name>')
-def hello_user(name):
-    if name=='admin':
-        return redirect(url_for('hello_admin'))
-    else:
-        return redirect(url_for('hello_guest',guest=name))
 
 if __name__ == '__main__':
     app.run()
